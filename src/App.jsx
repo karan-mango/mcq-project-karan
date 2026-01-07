21dc8b0f073438614ef3ca120415e77e5d182abd
sanjay <127958856+karan-mango@users.noreply.github.com>
2026-01-07 08:04:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Browser from "./pages/Browser";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Content from "./pages/Content";
 import Quiz from "./pages/Quiz";
 import QuestionBank from "./pages/QuestionBank";
